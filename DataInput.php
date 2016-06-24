1a70885743680b051a4d4266121a58de768660d6
dell user <i.panagiotou@rgu.ac.uk>
2016-06-24 11:11:50 +0000

    0

--- DataInput.php
+++ DataInput.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <?php
 /**
  * Created by PhpStorm.
@@ -7,3 +6,24 @@
  * Time: 11:06
  */
 ?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Data Input</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<h1>Please enter the following details</h1>
+<h3>Form</h3>
+    <br><br>
+    <form method="post" action="login.php">
+        <label>Username:</label><br>
+        <input type="text" name="username" placeholder="username" /><br><br>
+        <label>Password:</label><br>
+        <input type="password" name="password" placeholder="password" /><br><br>
+        <input type="submit" name="submit" value="login" />
+    </form>
+
+</div>
+</body>
+</html>
