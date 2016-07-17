88730242ac55b40115d0cfebb9b791dbba05e711
dell user <i.panagiotou@rgu.ac.uk>
2016-07-17 14:09:17 +0300

    0

--- Circuit.php
+++ Circuit.php
@@ -20,9 +20,9 @@
 <img src="planets.gif" width="145" height="126" alt="Planets" usemap="#planetmap">
 
 <map name="planetmap">
-    <area shape="rect" coords="0,0,82,126" alt="Sun" href="sun.htm">
-    <area shape="circle" coords="90,58,3" alt="Mercury" href="mercur.htm">
-    <area shape="circle" coords="124,58,8" alt="Venus" href="venus.htm">
+    <area shape="rect" coords="0,0,82,126" alt="Sun" href="DataInput.php">
+    <area shape="circle" coords="90,58,3" alt="Mercury" href="DataInput.php">
+    <area shape="circle" coords="124,58,8" alt="Venus" href="DataInput.php">
 </map>
 
 
