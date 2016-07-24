9aa956475f950e5c3e82ee8e626975661b5f02d1
dell user <i.panagiotou@rgu.ac.uk>
2016-07-24 19:20:21 +0300

    0

--- Circuit.php
+++ Circuit.php
@@ -23,9 +23,6 @@ include("dbconnect.php");
             //.parent().effect('highlight', {color: '#C7F464'}, 2000);
             alert("Hello World")
         }
-        function bigImg(x) {
-            x.style.color = "red";
-        }
     </script>
 </head>
 
@@ -43,10 +40,10 @@ include("dbconnect.php");
     <area shape="circle" coords="90,58,3" alt="Mercury" href="DataInput.php">
     <area shape="circle" coords="124,58,8" alt="Venus" href="DataInput.php">
     <area shape="poly" coords="271,177,267,185,282,191,284,184" alt="Block1" id="area1"
-          onmouseover="myFunction()"
+<!--          onmouseover="myFunction()"-->
           href="DataInput.php" />
     <area shape="poly" coords="286,183,282,191,295,196,298,190" alt="Block2"
-          onmouseover="bigImg(this)"
+          onmouseover="myFunction()"
           href="DataInput.php" />
 </map>
 
