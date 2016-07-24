c5d234766642e6a1d7504a4b6754590f6e37a9db
dell user <i.panagiotou@rgu.ac.uk>
2016-07-24 19:36:40 +0300

    0

--- Circuit.php
+++ Circuit.php
@@ -45,9 +45,8 @@ include("dbconnect.php");
     <area shape="rect" coords="0,0,82,126" alt="Sun" href="DataInput.php">
     <area shape="circle" coords="90,58,3" alt="Mercury" href="DataInput.php">
     <area shape="circle" coords="124,58,8" alt="Venus" href="DataInput.php">
-    <area id="area1" shape="poly" coords="271,177,267,185,282,191,284,184" alt="Block1"
-            data-maphilight='{"strokeColor":"0000ff","strokeWidth":5,"fillColor":"00ff00","fillOpacity":0.6}'
-          href="DataInput.php" />
+    <area shape="poly" coords="271,177,267,185,282,191,284,184" alt="Block1" href="DataInput.php"
+          data-maphilight="{'strokeColor':'0000ff','strokeWidth':5,'fillColor':'00ff00','fillOpacity':0.6}" />
     <area shape="poly" coords="286,183,282,191,295,196,298,190" alt="Block2"
           onmouseover="myFunction()"
           href="DataInput.php" />
