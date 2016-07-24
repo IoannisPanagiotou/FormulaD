e6e0d9e1a4df8057d7262bd46c12470ed1de31f8
dell user <i.panagiotou@rgu.ac.uk>
2016-07-24 23:56:57 +0300

    0

---
+++ Help.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dell user
+ * Date: 24/7/2016
+ * Time: 23:55
+ */
+?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Data Input</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+    <link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="unsemantic-grid-responsive-tablet.css">
+    <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1"
+    />
+</head>
+<body>
+Help
+</body>
+</html>
