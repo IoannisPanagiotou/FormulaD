0d685261973a4a59adb97cc373c35de72792dd76
dell user <i.panagiotou@rgu.ac.uk>
2016-07-24 18:13:07 +0300

    0

--- Circuit.php
+++ Circuit.php
@@ -20,7 +20,7 @@ include("dbconnect.php");
     />
     <script>
         function myFunction() {
-            document.getElementById("area1").style.color = "#ff0000";
+            document.body.style.backgroundColor="red"
             //alert("Hello World")
         }
     </script>
