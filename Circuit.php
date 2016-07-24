8a2dd0e8085e3acfa49e209356980b049ad73073
dell user <i.panagiotou@rgu.ac.uk>
2016-07-24 20:32:29 +0300

    0

--- Circuit.php
+++ Circuit.php
@@ -18,13 +18,113 @@ include("dbconnect.php");
         name="viewport"
         content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1"
     />
-    <script type="text/javascript" src="jquery.min.js"></script>
+    <script>
+
+        // stores the device context of the canvas we use to draw the outlines
+        // initialized in myInit, used in myHover and myLeave
+        var hdc;
+
+        // shorthand func
+        function byId(e){return document.getElementById(e);}
+
+        // takes a string that contains coords eg - "227,307,261,309, 339,354, 328,371, 240,331"
+        // draws a line from each co-ord pair to the next - assumes starting point needs to be repeated as ending point.
+        function drawPoly(coOrdStr)
+        {
+            var mCoords = coOrdStr.split(',');
+            var i, n;
+            n = mCoords.length;
+
+            hdc.beginPath();
+            hdc.moveTo(mCoords[0], mCoords[1]);
+            for (i=2; i<n; i+=2)
+            {
+                hdc.lineTo(mCoords[i], mCoords[i+1]);
+            }
+            hdc.lineTo(mCoords[0], mCoords[1]);
+            hdc.stroke();
+        }
+
+        function drawRect(coOrdStr)
+        {
+            var mCoords = coOrdStr.split(',');
+            var top, left, bot, right;
+            left = mCoords[0];
+            top = mCoords[1];
+            right = mCoords[2];
+            bot = mCoords[3];
+            hdc.strokeRect(left,top,right-left,bot-top);
+        }
+
+        function myHover(element)
+        {
+            var hoveredElement = element;
+            var coordStr = element.getAttribute('coords');
+            var areaType = element.getAttribute('shape');
+
+            switch (areaType)
+            {
+                case 'polygon':
+                case 'poly':
+                    drawPoly(coordStr);
+                    break;
+
+                case 'rect':
+                    drawRect(coordStr);
+            }
+        }
+
+        function myLeave()
+        {
+            var canvas = byId('myCanvas');
+            hdc.clearRect(0, 0, canvas.width, canvas.height);
+        }
+
+        function myInit()
+        {
+            // get the target image
+            var img = byId('img-imgmap201293016112');
+
+            var x,y, w,h;
+
+            // get it's position and width+height
+            x = img.offsetLeft;
+            y = img.offsetTop;
+            w = img.clientWidth;
+            h = img.clientHeight;
+
+            // move the canvas, so it's contained by the same parent as the image
+            var imgParent = img.parentNode;
+            var can = byId('myCanvas');
+            imgParent.appendChild(can);
+
+            // place the canvas in front of the image
+            can.style.zIndex = 1;
+
+            // position it over the image
+            can.style.left = x+'px';
+            can.style.top = y+'px';
+
+            // make same size as the image
+            can.setAttribute('width', w+'px');
+            can.setAttribute('height', h+'px');
+
+            // get it's context
+            hdc = can.getContext('2d');
+
+            // set the 'default' values for the colour/width of fill/stroke operations
+            hdc.fillStyle = 'red';
+            hdc.strokeStyle = 'red';
+            hdc.lineWidth = 2;
+        }
+    </script>
+    <!--<script type="text/javascript" src="jquery.min.js"></script>
     <script type="text/javascript" src="jquery.maphilight.min.js"></script>
     <script type="text/javascript">
         $(function () {
             $('.map').maphilight();
         });
-    </script>
+    </script>-->
         <!--/*$(function() {
             $('#circuit').maphilight();
         });*/
@@ -48,8 +148,8 @@ include("dbconnect.php");
     </script>-->
 </head>
 
-<body>
-
+<body onload='myInit()'>
+<canvas id='myCanvas'></canvas>
 <h2>Click on the corresponding block to enter the data</h2>
 <br>
 
@@ -61,7 +161,7 @@ include("dbconnect.php");
     <area shape="rect" coords="0,0,82,126" alt="Sun" href="DataInput.php">
     <area shape="circle" coords="90,58,3" alt="Mercury" href="DataInput.php">
     <area shape="circle" coords="124,58,8" alt="Venus" href="DataInput.php">
-    <area shape="poly" coords="271,177,267,185,282,191,284,184" alt="" href="DataInput.php"/>
+    <area shape="poly" onmouseover='myHover(this);' onmouseout='myLeave();' coords="271,177,267,185,282,191,284,184" alt="" href="DataInput.php"/>
 <!--          style="outline:none;" target="_self" data-maphilight='{"stroke":false,"fillColor":"cbf68c","fillOpacity":0.6}'-->
 <!--          data-maphilight="{'strokeColor':'0000ff','strokeWidth':5,'fillColor':'00ff00','fillOpacity':0.6}" -->
 <!--    style="filter:alpha(opacity=20);-moz-opacity:0.2" onMouseover="makevisible(this,0)" onMouseout="makevisible(this,1)" -->
