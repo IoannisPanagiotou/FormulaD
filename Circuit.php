c2f5f75fcbea8cc556ef5f1b5afb3d7f122c991e
dell user <i.panagiotou@rgu.ac.uk>
2016-07-24 17:52:31 +0300

    0

--- Circuit.php
+++ Circuit.php
@@ -19,7 +19,7 @@ include("dbconnect.php");
         content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1"
     />
     <script>
-        click: function myFunction() {
+        hover: function myFunction() {
         .parent().effect('highlight', {color: '#C7F464'}, 2000);
         }
     </script>
@@ -39,7 +39,7 @@ include("dbconnect.php");
     <area shape="circle" coords="90,58,3" alt="Mercury" href="DataInput.php">
     <area shape="circle" coords="124,58,8" alt="Venus" href="DataInput.php">
     <area shape="poly" coords="271,177,267,185,282,191,284,184" alt="Block1"
-          id="demo" onclick="myFunction()"
+          id="demo" onmouseover="myFunction()"
           href="DataInput.php" />
     <area shape="poly" coords="286,183,282,191,295,196,298,190" alt="Block2"
           onmouseover="writeText('The Sun and the gas giant planets like Jupiter are by far.')"
