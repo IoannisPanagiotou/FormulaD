a8a2b763f9de9490907b6f451578543e5e518d85
dell user <i.panagiotou@rgu.ac.uk>
2016-07-29 19:15:27 +0300

    0

--- ShowResults.php
+++ ShowResults.php
@@ -34,7 +34,7 @@ include("dbconnect.php");
             $min=2; //$_REQUEST["min"];
             $max=8; //$_REQUEST["max"];
             echo $min;
-            echo "\r\n";
+            echo "\n";
             echo $max;
             echo "<pre>Gear  Number  CornerHit  WPcorner  WPshortshift  TotalWearPoints  Probability</pre>";
 
