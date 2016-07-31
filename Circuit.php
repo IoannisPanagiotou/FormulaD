f6e701d33f7af252f200c225997ab87c83622838
dell user <i.panagiotou@rgu.ac.uk>
2016-07-31 23:41:41 +0300

    0

--- Circuit.php
+++ Circuit.php
@@ -46,33 +46,7 @@ include("dbconnect.php");
         });
     </script>
 
-    <script>
-        // Get the modal
-        var modal = document.getElementById('myModal');
 
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks the button, open the modal
-        btn.onclick = function() {
-            modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-    </script>
 
 
 </head>
@@ -83,7 +57,7 @@ include("dbconnect.php");
 
 
 <div class="cc-contactpop fixed">
-    <h1>Do you like this app? <span>Rate us!</span></h1>
+    <h1>Do you like this app? <span>Rate us!</span> [X]</h1>
     <div class="renseignement">
         <i class="fa fa-times"></i>
         <p>If you’d like to send us feedback or have any question, please let us know how well we did! </p>
@@ -109,30 +83,6 @@ include("dbconnect.php");
 <h3><a href="Help.php">Help</a></h3>
 <br>
 
-<h2>Animated Modal with Header and Footer</h2>
-
-<!-- Trigger/Open The Modal -->
-<button id="myBtn">Open Modal</button>
-
-<!-- The Modal -->
-<div id="myModal" class="modal">
-
-    <!-- Modal content -->
-    <div class="modal-content">
-        <div class="modal-header">
-            <span class="close">Χ</span>
-            <h2>Modal Header</h2>
-        </div>
-        <div class="modal-body">
-            <p>Some text in the Modal Body</p>
-            <p>Some other text...</p>
-        </div>
-        <div class="modal-footer">
-            <h3>Modal Footer</h3>
-        </div>
-    </div>
-
-</div>
 
 <main>
 
