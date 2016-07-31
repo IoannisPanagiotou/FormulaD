c35f5e99319e6e124a15c91bdc529b1605186bb8
dell user <i.panagiotou@rgu.ac.uk>
2016-07-31 23:18:54 +0300

    0

--- Circuit.php
+++ Circuit.php
@@ -23,10 +23,54 @@ include("dbconnect.php");
             return confirm('Are you sure you want to choose this block?')
         }
         </script>
-
+    
+    <script type="text/javascript">
+        $( document ).ready(function() {
+            // let's Hide Items
+            $( ".cc-contactpop form").hide();
+            $( ".cc-contactpop i").hide();
+            $( ".cc-contactpop p").hide();
+            // Show on elements ".slideDown"
+            $( ".cc-contactpop" ).click(function() {
+                $( ".cc-contactpop form").slideDown(500)
+                $( ".cc-contactpop p").slideDown(500)
+                $( ".cc-contactpop i").slideDown(500)
+            });
+            // Close Hidden Part
+            $( ".cc-contactpop i" ).click(function() {
+                $(this).slideUp(500)
+                $( ".cc-contactpop p").slideUp(500)
+                $( ".cc-contactpop form").slideUp(500)
+                return false
+            });
+        });
+    </script>
 </head>
 
 <body>
+
+<div class="cc-contactpop fixed">
+    <h1>Do you need Help? <span>Get in touch.</span></h1>
+    <div class="renseignement">
+        <i class="fa fa-times"></i>
+        <p>If you’d like to send us feedback, want to be become an author or have any other question/query, please fill out the form below and we will get back to you as soon as possible. </p>
+    </div>
+    <form method="post">
+        <div class="form-group">
+            <input type="text" name="name" placeholder="Your name"/>
+        </div>
+        <div class="form-group">
+            <input type="email" name="email" placeholder="E-mail"/>
+        </div>
+        <div class="form-group">
+            <input type="text" name="phone" placeholder="Phone number"/>
+        </div>
+        <div class="form-group">
+            <textarea id="message" type="text" placeholder="Message"></textarea>
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+</div>
 
 <h2>Click on the corresponding block to enter the data</h2>
 <h3><a href="Help.php">Help</a></h3>
