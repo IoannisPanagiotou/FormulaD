6e7d69bd65001e3ea9b19f57c3755e838d5ec92a
dell user <i.panagiotou@rgu.ac.uk>
2016-07-31 23:38:36 +0300

    0

--- Circuit.php
+++ Circuit.php
@@ -79,30 +79,7 @@ include("dbconnect.php");
 
 <body>
 
-<h2>Animated Modal with Header and Footer</h2>
 
-<!-- Trigger/Open The Modal -->
-<button id="myBtn">Open Modal</button>
-
-<!-- The Modal -->
-<div id="myModal" class="modal">
-
-    <!-- Modal content -->
-    <div class="modal-content">
-        <div class="modal-header">
-            <span class="close">Χ</span>
-            <h2>Modal Header</h2>
-        </div>
-        <div class="modal-body">
-            <p>Some text in the Modal Body</p>
-            <p>Some other text...</p>
-        </div>
-        <div class="modal-footer">
-            <h3>Modal Footer</h3>
-        </div>
-    </div>
-
-</div>
 
 
 <div class="cc-contactpop fixed">
@@ -132,6 +109,30 @@ include("dbconnect.php");
 <h3><a href="Help.php">Help</a></h3>
 <br>
 
+<h2>Animated Modal with Header and Footer</h2>
+
+<!-- Trigger/Open The Modal -->
+<button id="myBtn">Open Modal</button>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+    <!-- Modal content -->
+    <div class="modal-content">
+        <div class="modal-header">
+            <span class="close">Χ</span>
+            <h2>Modal Header</h2>
+        </div>
+        <div class="modal-body">
+            <p>Some text in the Modal Body</p>
+            <p>Some other text...</p>
+        </div>
+        <div class="modal-footer">
+            <h3>Modal Footer</h3>
+        </div>
+    </div>
+
+</div>
 
 <main>
 
