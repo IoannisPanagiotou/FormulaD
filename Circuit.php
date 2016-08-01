d070308dad7baa704aa5f95865b601d46d665b9c
dell user <i.panagiotou@rgu.ac.uk>
2016-08-02 00:31:33 +0300

    0

--- Circuit.php
+++ Circuit.php
@@ -113,7 +113,7 @@ include("dbconnect.php");
             <a href="#close" title="Close" class="close">X</a>
             <h2>Take part in our questionnaire</h2>
             <p>This is a sample modal box that can be created using the powers of CSS3.</p>
-            <p>You could do a lot of things here like have a pop-up ad that shows when your website loads, or create a login/register form for users.</p>
+            <h3><a href="Questionnaire.php">Questionnaire</a></h3>
         </div>
     </div>
 
