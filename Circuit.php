8d3b1f9202ed91a1aea209750ffff002ab5b996f
dell user <i.panagiotou@rgu.ac.uk>
2016-08-04 15:37:08 +0300

    0

--- Circuit.php
+++ Circuit.php
@@ -76,14 +76,15 @@ include("dbconnect.php");
     <div class="renseignement">
         <i class="fa fa-times"></i>
         <p>Whether you like our app or not, please let us know how well we did! </p>
+        <div class="rating">
+            <span><input type="radio" name="rating" id="str5" value="5"><label for="str5"></label></span>
+            <span><input type="radio" name="rating" id="str4" value="4"><label for="str4"></label></span>
+            <span><input type="radio" name="rating" id="str3" value="3"><label for="str3"></label></span>
+            <span><input type="radio" name="rating" id="str2" value="2"><label for="str2"></label></span>
+            <span><input type="radio" name="rating" id="str1" value="1"><label for="str1"></label></span>
+        </div>
     </div>
-    <div class="rating">
-        <span><input type="radio" name="rating" id="str5" value="5"><label for="str5"></label></span>
-        <span><input type="radio" name="rating" id="str4" value="4"><label for="str4"></label></span>
-        <span><input type="radio" name="rating" id="str3" value="3"><label for="str3"></label></span>
-        <span><input type="radio" name="rating" id="str2" value="2"><label for="str2"></label></span>
-        <span><input type="radio" name="rating" id="str1" value="1"><label for="str1"></label></span>
-    </div>
+
     <!--<form method="post">
         <div class="form-group">
             <input type="text" name="name" placeholder="Your name"/>
