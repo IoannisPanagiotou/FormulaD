7adbec9048985113c4838cb180d406ddd5200779
dell user <i.panagiotou@rgu.ac.uk>
2016-08-04 15:51:13 +0300

    0

--- Circuit.php
+++ Circuit.php
@@ -47,7 +47,7 @@ include("dbconnect.php");
     </script>
 
 
-    
+
 
 </head>
 
@@ -59,22 +59,22 @@ include("dbconnect.php");
     <div class="renseignement">
         <i class="fa fa-times"></i>
         <p>Whether you like our app or not, please let us know how well we did! </p>
-        <span class="rating">
-    <input type="radio" class="rating-input"
-           id="rating-input-1-5" name="rating-input-1">
-    <label for="rating-input-1-5" class="rating-star"></label>
-    <input type="radio" class="rating-input"
-           id="rating-input-1-4" name="rating-input-1">
-    <label for="rating-input-1-4" class="rating-star"></label>
-    <input type="radio" class="rating-input"
-           id="rating-input-1-3" name="rating-input-1">
-    <label for="rating-input-1-3" class="rating-star"></label>
-    <input type="radio" class="rating-input"
-           id="rating-input-1-2" name="rating-input-1">
-    <label for="rating-input-1-2" class="rating-star"></label>
-    <input type="radio" class="rating-input"
-           id="rating-input-1-1" name="rating-input-1">
-    <label for="rating-input-1-1" class="rating-star"></label>
+     <span class="rating">
+        <input type="radio" class="rating-input"
+               id="rating-input-1-5" name="rating-input-1"/>
+        <label for="rating-input-1-5" class="rating-star"></label>
+        <input type="radio" class="rating-input"
+               id="rating-input-1-4" name="rating-input-1"/>
+        <label for="rating-input-1-4" class="rating-star"></label>
+        <input type="radio" class="rating-input"
+               id="rating-input-1-3" name="rating-input-1"/>
+        <label for="rating-input-1-3" class="rating-star"></label>
+        <input type="radio" class="rating-input"
+               id="rating-input-1-2" name="rating-input-1"/>
+        <label for="rating-input-1-2" class="rating-star"></label>
+        <input type="radio" class="rating-input"
+               id="rating-input-1-1" name="rating-input-1"/>
+        <label for="rating-input-1-1" class="rating-star"></label>
 </span>
     </div>
 
