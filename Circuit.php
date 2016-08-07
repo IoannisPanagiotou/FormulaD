484a7356d5c642fabd60346492ad7ef685f99046
dell user <i.panagiotou@rgu.ac.uk>
2016-08-07 19:23:28 +0300

    0

--- Circuit.php
+++ Circuit.php
@@ -43,10 +43,8 @@ include("dbconnect.php");
             });
         });
     </script>
-
-
-
-
+    
+    
 </head>
 
 <body>
@@ -59,24 +57,7 @@ include("dbconnect.php");
         <p>Whether you like our app or not, please let us know how well we did! </p>
     </div>
 
-    <form method="post">
-        <span class="rating">
-        <input type="radio" class="rating-input"
-               id="rating-input-1-5" name="rating-input-1"/>
-        <label for="rating-input-1-5" class="rating-star"></label>
-        <input type="radio" class="rating-input"
-               id="rating-input-1-4" name="rating-input-1"/>
-        <label for="rating-input-1-4" class="rating-star"></label>
-        <input type="radio" class="rating-input"
-               id="rating-input-1-3" name="rating-input-1"/>
-        <label for="rating-input-1-3" class="rating-star"></label>
-        <input type="radio" class="rating-input"
-               id="rating-input-1-2" name="rating-input-1"/>
-        <label for="rating-input-1-2" class="rating-star"></label>
-        <input type="radio" class="rating-input"
-               id="rating-input-1-1" name="rating-input-1"/>
-        <label for="rating-input-1-1" class="rating-star"></label>
-</span>
+
 
     </form>
 </div>
---
+++ RateUs.php
@@ -0,0 +1,45 @@
+<?php
+include("dbconnect.php");
+?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Questionnaire</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+    <link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="unsemantic-grid-responsive-tablet.css">
+    <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1"
+    />
+</head>
+
+<body>
+<form method="post">
+        <span class="rating">
+        <input type="radio" class="rating-input"
+               id="rating-input-1-5" name="rating-input-1"/>
+        <label for="rating-input-1-5" class="rating-star"></label>
+        <input type="radio" class="rating-input"
+               id="rating-input-1-4" name="rating-input-1"/>
+        <label for="rating-input-1-4" class="rating-star"></label>
+        <input type="radio" class="rating-input"
+               id="rating-input-1-3" name="rating-input-1"/>
+        <label for="rating-input-1-3" class="rating-star"></label>
+        <input type="radio" class="rating-input"
+               id="rating-input-1-2" name="rating-input-1"/>
+        <label for="rating-input-1-2" class="rating-star"></label>
+        <input type="radio" class="rating-input"
+               id="rating-input-1-1" name="rating-input-1"/>
+        <label for="rating-input-1-1" class="rating-star"></label>
+</span>
+</form>
+
+<a href="Circuit.php" title="Back to the Home page"><b>HOME</b></a> | <a href="#" title="To the top of this page"><b>BACK TO TOP</b></a>
+
+<footer>
+    <p>(c) 2016 Yannis Panagiotou</p>
+</footer>
+
+</body>
+</html>
