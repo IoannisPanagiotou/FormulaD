22347cb06087e759d2601ca4829cee25d7fff3fd
dell user <i.panagiotou@rgu.ac.uk>
2016-08-09 21:33:48 +0300

    0

--- Questionnaire.php
+++ Questionnaire.php
@@ -118,24 +118,24 @@ include("dbconnect.php");
 </form>
 
 <?
+    $q1 = $_POST["q1"];
+    $q2 = $_POST["q2"];
+    $q3 = $_POST["q3"];
+    $q4 = $_POST["q4"];
+    $q5 = $_POST["q5"];
+    $q6 = $_POST["q6"];
+    $q7 = $_POST["q7"];
+    $q8 = $_POST["q8"];
+    $q9 = $_POST["q9"];
+    $q10 = $_POST["q10"];
 
-/*}
-elseif($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $gear = $_POST["gear"];
-    $corner;
-    $WP = $_POST["WP"];
-    $prob;
-    $max;
-    $min;
-
-    $sql = "INSERT INTO Results (Gear, Corner, WearPoints, Probability, maximum, minimum) VALUES ('$gear','$corner','$WP','$prob','$max','$min')";
+    $sql = "INSERT INTO userevaluation (q1, q2, q3, q4, q5, q6, q7, q8, q9, q10) VALUES ('$q1','$q2','$q3','$q4','$q5','$q6','$q7','$q8','$q9','$q10')";
     if (mysqli_query($db, $sql)) {
     } else {
         echo "<br>Error: " . $sql . "<br>" . mysqli_error($db);
     }
-    header("location: ShowResults.php");
-}
-*/?>
+    //header("location: ShowResults.php");
+?>
 
 <a href="Circuit.php" title="Back to the Home page"><b>HOME</b></a> | <a href="#" title="To the top of this page"><b>BACK TO TOP</b></a>
 
