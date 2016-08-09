f61b6ff1e607a1306bf7f260b61b83f2b63d8eb0
dell user <i.panagiotou@rgu.ac.uk>
2016-08-09 17:34:56 +0300

    0

--- RateUs.php
+++ RateUs.php
@@ -35,9 +35,9 @@ include("dbconnect.php");
         <label for="rating-input-1-2" class="rating-star"></label>
         <input type="radio" class="rating-input"
                id="rating-input-1-1" name="rating-input-1"/>
-        <label for="rating-input-1-1" class="rating-star"></label><br><br><br>
-        <input type="submit" name="submit" value="Submit"/>
-    </span>
+        <label for="rating-input-1-1" class="rating-star"></label>
+    </span><br><br>
+    <input type="submit" name="submit" value="Submit"/>
 </form>
 
 <a href="Circuit.php" title="Back to the Home page"><b>HOME</b></a> | <a href="#" title="To the top of this page"><b>BACK
