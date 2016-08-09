ff8b43ad7c8a880a772f4eb9c585bb019e4b01b3
dell user <i.panagiotou@rgu.ac.uk>
2016-08-09 22:22:36 +0300

    0

--- RateUs.php
+++ RateUs.php
@@ -22,19 +22,19 @@ include("dbconnect.php");
     <textarea rows="4" cols="50" name="comment" placeholder="Enter your comments here..."></textarea> <br>
     <span class="rating">
         <input type="radio" class="rating-input"
-               id="rating-input-1-5" name="rating-input-1" value="1"/>
+               id="rating-input-1-5" name="rating-input-1" value="5"/>
         <label for="rating-input-1-5" class="rating-star"></label>
         <input type="radio" class="rating-input"
-               id="rating-input-1-4" name="rating-input-1" value="2"/>
+               id="rating-input-1-4" name="rating-input-1" value="4"/>
         <label for="rating-input-1-4" class="rating-star"></label>
         <input type="radio" class="rating-input"
                id="rating-input-1-3" name="rating-input-1" value="3"/>
         <label for="rating-input-1-3" class="rating-star"></label>
         <input type="radio" class="rating-input"
-               id="rating-input-1-2" name="rating-input-1" value="4"/>
+               id="rating-input-1-2" name="rating-input-1" value="2"/>
         <label for="rating-input-1-2" class="rating-star"></label>
         <input type="radio" class="rating-input"
-               id="rating-input-1-1" name="rating-input-1" value="5"/>
+               id="rating-input-1-1" name="rating-input-1" value="1"/>
         <label for="rating-input-1-1" class="rating-star"></label>
     </span><br><br>
     <input type="submit" name="submit" value="Submit"/>
