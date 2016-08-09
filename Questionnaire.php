4b112f1834b9360b113cb823de812e5d2fc10ba0
dell user <i.panagiotou@rgu.ac.uk>
2016-08-09 22:09:11 +0300

    0

--- Questionnaire.php
+++ Questionnaire.php
@@ -116,27 +116,7 @@ include("dbconnect.php");
     <input type="submit" name="submit" value="Submit" />
 </form>
 
-<?
-    $q1 = $_POST["q1"];
-    $q2 = $_POST["q2"];
-    $q3 = $_POST["q3"];
-    $q4 = $_POST["q4"];
-    $q5 = $_POST["q5"];
-    $q6 = $_POST["q6"];
-    $q7 = $_POST["q7"];
-    $q8 = $_POST["q8"];
-    $q9 = $_POST["q9"];
-    $q10 = $_POST["q10"];
 
-echo $q1;
-
-$sql = "INSERT INTO userevaluation (Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10) VALUES ('$q1','$q2','$q3','$q4','$q5','$q6','$q7','$q8','$q9','$q10')";
-    if (mysqli_query($db, $sql)) {
-    } else {
-        echo "<br>Error: " . $sql . "<br>" . mysqli_error($db);
-    }
-    //header("location: ShowResults.php");
-?>
 
 <a href="Circuit.php" title="Back to the Home page"><b>HOME</b></a> | <a href="#" title="To the top of this page"><b>BACK TO TOP</b></a>
 
--- ThankYou.php
+++ ThankYou.php
@@ -24,6 +24,27 @@ include("dbconnect.php");
 </head>
 <body>
 
+<?
+$q1 = $_POST["q1"];
+$q2 = $_POST["q2"];
+$q3 = $_POST["q3"];
+$q4 = $_POST["q4"];
+$q5 = $_POST["q5"];
+$q6 = $_POST["q6"];
+$q7 = $_POST["q7"];
+$q8 = $_POST["q8"];
+$q9 = $_POST["q9"];
+$q10 = $_POST["q10"];
+
+echo $q1;
+
+$sql = "INSERT INTO userevaluation (Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10) VALUES ('$q1','$q2','$q3','$q4','$q5','$q6','$q7','$q8','$q9','$q10')";
+if (mysqli_query($db, $sql)) {
+} else {
+    echo "<br>Error: " . $sql . "<br>" . mysqli_error($db);
+}
+//header("location: ShowResults.php");
+?>
 
 <h2 id="thanks">Your feedback has been sent successfully!</h2>
 
