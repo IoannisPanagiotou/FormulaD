c70b06113b84f1f1ef5ff181ea7fe41745f5e01a
dell user <i.panagiotou@rgu.ac.uk>
2016-08-15 17:53:09 +0100

    If this goes wrog its Mikes fault...

--- ThankYou.php
+++ ThankYou.php
@@ -5,7 +5,7 @@ include("dbconnect.php");
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Questionnaire</title>
+    <title>Thank You</title>
     <link rel="stylesheet" href="style.css" type="text/css" />
     <link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="unsemantic-grid-responsive-tablet.css">
