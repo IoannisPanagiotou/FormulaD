71a1bead7334497142a1445f148de3492525411e
dell user <i.panagiotou@rgu.ac.uk>
2016-08-16 17:54:32 +0100

    If this goes wrog its Mikes fault...

--- Questionnaire.php
+++ Questionnaire.php
@@ -15,7 +15,8 @@ include("dbconnect.php");
 </head>
 <body>
 
-<h2>Please answer the following questions where 1:Strongly Disagree 5:Strongly Agree</h2>
+<h2>Please answer the following questions</h2>
+<h4>1:Strongly Disagree 5:Strongly Agree</h4>
 <h3><a href="Help.php">Help</a></h3>
 <br>
 
